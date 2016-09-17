027948335975934d506a606074cb68b9974dbefe
anton164 <anton_abilov@hotmail.com>
2016-09-17 20:20:04 +0200

    Map updates

---
+++ app/scripts/controllers/direction.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('SirklerApp')
+  .controller('DirectionController', function($scope, uiGmapGoogleMapApi) {
+
+  });
--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -1,6 +1,46 @@
 'use strict';
 
 angular.module('SirklerApp')
-  .controller('MapController', function ($scope) {
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+  .controller('MapController', function($scope, uiGmapGoogleMapApi) {
+    var origin = { // Høvik
+      latitude: 59.901401,
+      longitude: 10.557090
+    }, destination = { // Oslo
+      latitude: 59.91139,
+      longitude: 10.7522
+    };
+    $scope.map = {
+      control: {},
+      center: origin,
+      zoom: 14
+    };
+    $scope.directions = {};
+    $scope.directions.origin = origin;
+    $scope.directions.destination = destination;
+
+    var directionsDisplay, directionsService, geocoder;
+    uiGmapGoogleMapApi.then(function (maps) {
+      directionsDisplay = new google.maps.DirectionsRenderer();
+      directionsService = new google.maps.DirectionsService();
+      geocoder = new google.maps.Geocoder();
+
+
+      var request = {
+        origin: 'Høvik, Norge',
+        destination: 'Oslo, Norge',
+        travelMode: google.maps.DirectionsTravelMode.DRIVING
+      };
+
+      directionsService.route(request, function (response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+          directionsDisplay.setMap($scope.map.control.getGMap());
+          directionsDisplay.setPanel(document.getElementById('directionsList'));
+          $scope.directions.showList = true;
+        } else {
+          alert('Google route unsuccesfull!');
+        }
+      });
+    });
   });
+
